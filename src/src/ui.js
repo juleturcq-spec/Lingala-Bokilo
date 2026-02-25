92d173f8f58bec50e07f27c2dc0312973556f18c
juleturcq-spec <ju.leturcq@hotmail.be>
2026-02-25 02:26:10 +0100

    Delete src directory

--- src/src/ui.js
+++
@@ -1,9 +0,0 @@
-export function el(html){
-  const template = document.createElement("template");
-  template.innerHTML = html.trim();
-  return template.content.firstChild;
-}
-
-export function toast(message){
-  alert(message);
-}
