bbea2e8e84e3cd572b13f3a6937a1a5d15617da7
juleturcq-spec <ju.leturcq@hotmail.be>
2026-02-25 02:55:58 +0100

    Delete source/src/source/src directory

--- source/src/source/src/ui.js
+++
@@ -1,9 +0,0 @@
-export function el(html){
-  const t = document.createElement("template");
-  t.innerHTML = html.trim();
-  return t.content.firstChild;
-}
-
-export function toast(msg){
-  alert(msg);
-}
